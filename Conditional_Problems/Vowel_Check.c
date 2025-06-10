b25ae5eca919bb8d27f8b110963a0b79b97599df
Musa Bin Khalid <musabinkhalid2121@gmail.com>
2025-06-10 23:01:16 +0530

    Update and rename Vowel(Switch).c to Vowel_Check.c

---
+++ Conditional_Problems/Vowel_Check.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+
+	char ch;
+	
+	printf("Enter a Character: ");
+	scanf(" %c", &ch);
+	
+	switch(ch){
+		
+		case 'A':
+        case 'a':
+        case 'E':
+        case 'e':
+        case 'I':
+        case 'i':
+        case 'O':
+        case 'o':
+        case 'U':
+        case 'u':
+		 
+			printf("This is vowel");
+		break;
+		
+		default: printf("This is consonant");
+	}
+	
+return 0;
+}
